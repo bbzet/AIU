5f2dcaf9cbcd0fcac685dae4beae14644c34a35d
Baiastan <baastan569@gmail.com>
2023-11-27 12:11:38 +0600

    Added for checking if user inputed right or wrong locations checked only one time

--- helpingmartians.py
+++ helpingmartians.py
@@ -1,3 +1,4 @@
+import random
 distance = [200, 0, 200, 1, 4, 313, 9]
 first_data1 = distance[0]
 first_data2 = distance[2]
@@ -6,4 +7,13 @@ weight = 0
 
 
 for _ in range(3):
-    kilometers1 = int(input("Enter the kilometer mark where the box can be buried: "))
+    kilometers = int(input("Enter the kilometer mark where the box can be buried: "))
+    weight += distance[kilometers]
+
+if weight == 713:
+    print(f"Congratulations! The weight is {weight} kilograms. You have marked the right locations of cargo.")
+else:
+    print(f'{weight} kilograms are not the right weight. You marked wrong locations, and the boxes changed their locations. Try it again.')
+    random.shuffle(distance)
+
+    weight = 0
